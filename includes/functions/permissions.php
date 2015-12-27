449db503d93d5e562bd795fc638f35191a37354d
Joar Classon <joarc@joarc.se>
2015-12-27 19:50:18 +0100

    started work on some permissions functions

--- includes/functions/permissions.php
+++ includes/functions/permissions.php
@@ -1,18 +1,36 @@
 <?php
 
 function listUserPermissions($userid) {
-
+  if (is_double($userid)) {
+    return json_decode(executeResult("SELECT `permissions` FROM `users` WHERE `id`=?", array($userid))[0], true);
+  } else {
+    return false;
+  }
 }
 
 function grantUserPermissions($userid, $permissions) {
   if (is_double($userid) && is_array($permissions)) {
-
+    $userpermissions = json_decode(executeResult("SELECT `permissions` FROM `nf_users` WHERE `id`=?", array($userid))[0], true);
+    foreach($permissions as $value) {
+      array_push($userpermissions, $value);
+    }
+    execute("UPDATE `nf_users` SET `permissions`=? WHERE `id`=?", array(json_encode($userpermissions), $userid));
+  } else {
+    return false;
   }
 }
 
 function removeUserPermissions($userid, $permissions) {
   if (is_double($userid) && is_array($permissions)) {
-
+    $userpermissions = json_decode(executeResult("SELECT `permissions` FROM `nf_users` WHERE `id`=?", array($userid))[0], true);
+    foreach($userpermissions as $key => $value) {
+      if (in_array($value, $permissions)) {
+        unset($userpermissions[$key]);
+      }
+    }
+    execute("UPDATE `nf_users` SET `permissions`=? WHERE `id`=?", array(json_encode($userpermissions), $userid));
+  } else {
+    return false;
   }
 }
 
