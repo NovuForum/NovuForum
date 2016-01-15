94ff0ac78329c72097bde2a78baed9402d0d4bf2
Joar Classon <joarc@joarc.se>
2016-01-15 11:38:25 +0100

    more additions to forumlist

--- includes/functions/posts.php
+++ includes/functions/posts.php
@@ -11,25 +11,41 @@ function newPost($topicid, $content) {
 }
 
 // TODO: REUSE THIS CODE
-function getPosts($page) {
-  if ($page == -1) {
-    $sql = "SELECT * FROM `blogg` ORDER BY id DESC";
-    $args = array();
-    return executeResults($sql, $args);
-  }
-  $pagelength = 10;
+function getPostsByPage($page) {
   if (is_numeric($page)) {
     if ($page == 1) {
-      $sql = "SELECT * FROM `blogg` ORDER BY id DESC LIMIT 0,10 ";
+      $sql = "SELECT * FROM `nf_posts` ORDER BY id DESC LIMIT 0,10 ";
       $args = array();
       return executeResults($sql, $args);
     } else {
       $page = $page * 10 - 10;
-      $sql = "SELECT * FROM `blogg` ORDER BY `id` DESC LIMIT $page,10";
+      $sql = "SELECT * FROM `nf_posts` ORDER BY `id` DESC LIMIT $page,10";
       $args = array();
       return executeResults($sql, $args);
     }
   } else {
-    return array("title" => "Failed getting posts", "linkid" => "failed", "description" => "Failed getting posts, please review the code or contact joarc@joarc.se");
+    return array("title" => "Failed getting posts", "linkid" => "failed", "description" => "Failed getting posts, page variable isnt number. Please notify developers!");
   }
+}
+
+function countTopicsByForumId($id) {
+  $i = 0;
+  $previd = -1;
+  foreach (executeResults("SELECT * FROM `nf_posts` WHERE `forumid`=?", array($id)) as $value) {
+    if ($value['topicid'] == $previd): continue; else: $previd = $value['topicid'] endif;
+    $i++;
+  }
+  return $i;
+}
+
+function countPostsByForumId($id) {
+  $i = 0;
+  foreach (executeResults("SELECT * FROM `nf_posts` WHERE `forumid`=?", array($id)) as $value) {
+    $i++;
+  }
+  return $i;
+}
+
+function getLatestPostByForumId($id) {
+  return executeResult("SELECT `title` FROM `nf_posts` WHERE `forumid`=? ORDER BY DESC LIMIT 1")[0];
 }
--- includes/pages/forumlist.parser.php
+++ includes/pages/forumlist.parser.php
@@ -22,12 +22,24 @@ $object['type'] = object type
   </tr>
   <?php foreach($forumslist as $key => $object) { ?>
     <?php if ($object['type'] == 1) { ?>
-    <tr>
-      <td colspan="6" class="forum-name"><?= $object['title'] ?><br><small><?= $object['content'] ?></small></td>
-      <td colspan="1" class="forum-topics"><?= countTopics($object['id']) ?></td>
-      <td colspan="1" class="forum-posts"><?= countPosts($object['id']) ?></td>
-      <td colspan="3" class="forum-latest"><?= getLatestPostsFromForums($object['id']) ?></td>
-    </tr>
+      <tr>
+        <td colspan="6" class="forum-name"><a href="/forum/<?= $object['id'] ?>"><?= $object['title'] ?><br><small><?= $object['content'] ?></small></a></td>
+        <td colspan="1" class="forum-topics"><?= countTopics($object['id']) ?></td>
+        <td colspan="1" class="forum-posts"><?= countPosts($object['id']) ?></td>
+        <td colspan="3" class="forum-latest"><?= getLatestPostByForumId($object['id']) ?></td>
+      </tr>
+    <?php } else if ($object['type'] == 2) { ?>
+      <tr>
+        <td colspan="11" class="forum-divider"></td>
+      </tr>
+    <?php } else if ($object['type'] == 3) { ?>
+      <tr>
+        <td colspan="11" class="forum-divider"><?= $object['title'] ?><?php if ($object['content'] != ""): ?><br><small><?= $object['content']?></small><?php endif; ?></td>
+      </tr>
+    <?php } else if ($object['type'] == 4) { ?>
+      <tr>
+        <td colspan="11" class="forum-link"><a href="<?= $object['url'] ?>"><?= $object['title'] ?><?php if ($object['content'] != ""): ?><br><small><?= $object['content']?></small><?php endif; ?></a></td>
+      </tr>
     <?php } ?>
   <?php } ?>
 </table>
